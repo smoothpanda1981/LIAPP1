e99e96308bd021c9544b42a6424815ac5ddf44d1
Smooth Panda <smoothpanda1981@gmail.com>
2015-01-14 13:16:11 +0100

    all tables implemented

--- src/main/java/com/yan/wang/sprin/mvc/HelloController.java
+++ src/main/java/com/yan/wang/sprin/mvc/HelloController.java
@@ -1,6 +1,8 @@
 package com.yan.wang.sprin.mvc;
 
 import com.yan.wang.sprin.mvc.dao.Customer;
+import com.yan.wang.sprin.mvc.dao.CustomerVoucher;
+import com.yan.wang.sprin.mvc.dao.Voucher;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +21,7 @@ public class HelloController {
 		return "hello";
 	}
 
-	@RequestMapping(value = "page1", method = RequestMethod.GET)
+	@RequestMapping(value = "db", method = RequestMethod.GET)
 	public String page1(ModelMap model) {
 		model.addAttribute("message", "DB Status");
 
@@ -32,7 +34,6 @@ public class HelloController {
 			ResultSet resultSet;
 
 			resultSet = statement.executeQuery("select * from customer");
-
 			ResultSetMetaData meta   = resultSet.getMetaData();
 			/*
 			String[] columnsNames = new String[meta.getColumnCount()];
@@ -42,31 +43,45 @@ public class HelloController {
 				System.out.println(columnsNames[i-1]);
 			}
 			*/
-
 			List<Customer> customerList = new ArrayList<Customer>();
-
 			while (resultSet.next()) {
 				Customer customer = new Customer();
 				customer.setCustomer_id(resultSet.getInt(meta.getColumnName(1)));
 				customer.setEmail(resultSet.getString(meta.getColumnName(2)));
 				customerList.add(customer);
 			}
-
 			model.addAttribute("customerList", customerList);
 
 
-//			resultSet = statement.executeQuery("select * from voucher");
+			resultSet = statement.executeQuery("select * from voucher");
+			meta   = resultSet.getMetaData();
+			List<Voucher> voucherList = new ArrayList<Voucher>();
+			while (resultSet.next()) {
+				Voucher voucher = new Voucher();
+				voucher.setVoucher_id(resultSet.getInt(meta.getColumnName(1)));
+				voucher.setVoucher_text(resultSet.getString(meta.getColumnName(2)));
+				voucher.setCompany_name(resultSet.getString(meta.getColumnName(3)));
+				voucherList.add(voucher);
+			}
+			model.addAttribute("voucherList", voucherList);
 
-//			resultSet = statement.executeQuery("select * from customer_voucher");
 
+			resultSet = statement.executeQuery("select * from customer_voucher");
+			meta   = resultSet.getMetaData();
+			List<CustomerVoucher> customerVoucherList = new ArrayList<CustomerVoucher>();
+			while (resultSet.next()) {
+				CustomerVoucher customerVoucher = new CustomerVoucher();
+				customerVoucher.setId(resultSet.getInt(meta.getColumnName(1)));
+				customerVoucher.setCustomer_id(resultSet.getInt(meta.getColumnName(2)));
+				customerVoucher.setVoucher_id(resultSet.getInt(meta.getColumnName(3)));
+				customerVoucherList.add(customerVoucher);
+			}
+			model.addAttribute("customerVoucherList", customerVoucherList);
 
 		} catch (Exception e) {
 			System.err.println("ERROR: failed to load HSQLDB JDBC driver.");
 			e.printStackTrace();
 		}
-
-
-
 		return "DB_Status";
 	}
 
